0e0f9a5a5e6e7adbb1857aa39545fbb4b440379a
yoshitugu <yuki.38220512@gmail.com>
2019-10-26 04:02:58 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+ 
+ # ページごとにタイトルを返す
+ def full_title(page_name = "") # メソッドと引数の定義
+   base_title = "AttendanceApp" # 基本となるアプリケーション名を変数に代入
+   if page_name.empty?
+     base_title
+   else
+     page_name + " | " + base_title
+   end
+ end
 end
