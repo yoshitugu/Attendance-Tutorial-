de50b75c8921713de99b4911d5b082eaabf14396
yoshitugu <yuki.38220512@gmail.com>
2019-10-30 05:17:16 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規作成に成功しました。'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+  
+  private # 外部からしようできないようにするため
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
 end
