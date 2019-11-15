78bd5743893744c3d2511a4ba55f99ccc11a6116
yoshitugu <yuki.38220512@gmail.com>
2019-11-15 12:37:40 +0000

    Finish add-user-attendances

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,5 @@
 class AttendancesController < ApplicationController
+  UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
   
   def update
     @user = User.find(params[:user_id])
@@ -7,9 +8,15 @@ class AttendancesController < ApplicationController
     if @attendance.started_at.nil?
        if @attendance.update_attributes(started_at: Time.current.change(sec:0))
          flash[:info] = "おはようございます♡"
+       else
+        flash[:danger] = UPDATE_ERROR_MSG
+       end
+    elsif @attendance.finished_at.nil?
+       if @attendance.update_attributes(finished_at: Time.current.change(sec:0))
+     　  flash[:info] = "お疲れ様でした"
      　else
-        flash[:danger] = "勤怠登録に失敗しました。やり直してください。"
-     　 end
+     　flash[:danger] = UPDATE_ERROR_MSG
+       end  
     end
     redirect_to @user
   end    
